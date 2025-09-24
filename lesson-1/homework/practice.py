75a736313b84362b424e20fb6bd317b6e92ed7c4
abdulaziz <abdulaziz.axi01@gmail.com>
2025-09-24 13:19:17 +0500

    Create practice.py

---
+++ lesson-1/homework/practice.py
@@ -0,0 +1,35 @@
+# VAZIFA 1: Kvadratning perimetri va yuzi
+s = 5
+perimetr = 4 * s
+yuza = s * s
+print("1-vazifa:")
+print("kvadrat tomoni:", s, "perimetri:", perimetr, "yuzi:", yuza)
+print()
+
+# VAZIFA 2: Doiraning uzunligi (pi = 3.14)
+d = 10
+pi = 3.14
+uzunlik = pi * d
+print("2-vazifa:")
+print("diametr:", d, "doira uzunligi:", uzunlik)
+print()
+
+# VAZIFA 3: Ikkita sonning o'rtacha qiymati
+a = 8
+b = 12
+mean = (a + b) / 2
+print("3-vazifa:")
+print("a:", a, "b:", b, "o'rtacha qiymat:", mean)
+print()
+
+# VAZIFA 4: Yig'indi, ko'paytma va kvadratlar
+a = 3
+b = 4
+yigindi = a + b
+kopaytma = a * b
+a_kvadrat = a * a
+b_kvadrat = b * b
+print("4-vazifa:")
+print("a:", a, "b:", b)
+print("yig'indi:", yigindi, "ko'paytma:", kopaytma)
+print("a^2:", a_kvadrat, "b^2:", b_kvadrat)
